0d964db67367916dc4a4be3d44b56f97b239786f
{tajima} <{stummhpc0703@gmail.com}>
2022-09-12 13:17:00 +0900

    トップページ更新

--- html/index.js
+++
@@ -1,8 +0,0 @@
- jQuery(window).on('scroll', function(){
-    var stp = jQuery(window).scrollTop();
-    var bgp = stp / 1.5;
-  
-    if(bgp){
-      jQuery('.big-bg').css('background-position', 'center +'+ bgp + 'px');
-    }
-  });
