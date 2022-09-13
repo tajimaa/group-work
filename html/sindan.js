cd589daed2ac44a30a99d2115bccc9f9e55ed829
Dakue07 <yt22304014@ga.ttc.ac.jp>
2022-09-13 16:38:49 +0900

    結果を9個に増やしました

--- html/sindan.js
+++ html/sindan.js
@@ -1,8 +1,8 @@
 //問題と回答
 qa = new Array();
-qa[0] = ["今回購入検討中のpcの主な用途は何ですか？","主に自宅で、日常的な調べごとやオンラインショッピング、事務作業などに使いたい<br><br>","自宅での普段使いに加えて、学校や職場などへの外出先へ持ち運んで使いたい<br><br>","写真編集や動画制作、オンラインpcゲームなどで使いたい", 200, 500, 1000];
-qa[1] = ["今回購入を検討しているpcの予算はどれくらいですか?<br>","安いほうがいい<br><br>","中間ほどがいい<br><br>","値段は問わない<br>", 200, 500, 1000];
-qa[2] = ["以下の中で、最もあてはまるものを選んでください","せっかく買うなら最新のスペックや機能を搭載しているものがいい。<br><br>","スペックや性能も重要だが、値段も少し抑えたい。<br><br>","特にこだわりがない、できるだけ安いものがいい。<br>",1000, 500, 200];
+qa[0] = ["あなたが求めるpcの性能はどれくらいですか？","ノートpc。<br><br>","デスクトップpc。<br><br>","どちらでもよい。", 0, 100000, 50000];
+qa[1] = ["今回購入を検討しているpcの予算はどれくらいですか?<br>","安いほうがいい。<br><br>","中間ほどがいい。<br><br>","値段は問わない。<br>", 200, 500, 1000];
+qa[2] = ["以下の中で、最もあてはまるものを選んでください","日常的な調べごとやオンラインショッピング、事務作業などで使う最低限の性能のpc。。<br><br>","動画閲覧やコーディングなどで使う、中程度の性能のpc。<br><br>","写真編集や動画制作、オンラインpcゲームなどで使う高い性能のpc。<br>", 200, 500, 1000];
 
 
 //初期設定
@@ -41,7 +41,7 @@ function anser(num) {
     s += qa[count][num + q_sel];
 
     //ansers(合計スコア)のチェック
-    if (ansers > 100000) ansers = 100000; //上限値
+    //if (ansers > 100000) ansers = 100000; //上限値
     if (ansers <= -1) {
         game_over(); //不具合
     } else {
@@ -51,9 +51,9 @@ function anser(num) {
         if (count < qa.length) {
             sindan();
         }else {
-             //終了
-            if (ansers >= 2500) {
-                s = "超すげーはいぱぱそこん";
+             //終了　デスクトップpc表示
+            if (ansers >= 101500) {
+                s = "dpすげーはいぱ";
                 document.getElementById("text_q").innerHTML = s;
                 //次の選択肢
                 s = "【<a href = 'file:///C:/Users/koyama/Desktop/%E9%96%8B%E7%99%BA%E6%AE%B4%E6%89%93/group-work/html/index.html'>前のページに戻る</a>】";
@@ -61,24 +61,73 @@ function anser(num) {
                 s += "【<a href = ''>詳細を見る</a>】";
                 document.getElementById("text_s").innerHTML = s;
 
-            } else if(ansers >= 1700) {
-                s = "すげーぱそこん";
+            } else if(ansers >= 101000) {
+                s = "dpすげー";
                 document.getElementById("text_q").innerHTML = s;
                 //次の選択肢
                 s = "【<a href = 'file:///C:/Users/koyama/Desktop/%E9%96%8B%E7%99%BA%E6%AE%B4%E6%89%93/group-work/html/index.html'>前のページに戻る</a>】";
                 s += "【<a href = 'javascript:setReady()'>同じ問題を最初から</a>】";
                 s += "【<a href = ''>詳細を見る</a>】";
                 document.getElementById("text_s").innerHTML = s;
-            } else if(ansers >= 1200) {
-                s = "そこそこぱそこん";
+
+            } else if(ansers >= 100000) {
+                s = "dpそこそこぱそこん";
                 document.getElementById("text_q").innerHTML = s;
                 //次の選択肢
                 s = "【<a href = 'file:///C:/Users/koyama/Desktop/%E9%96%8B%E7%99%BA%E6%AE%B4%E6%89%93/group-work/html/index.html'>前のページに戻る</a>】";
                 s += "【<a href = 'javascript:setReady()'>同じ問題を最初から</a>】";
                 s += "【<a href = ''>詳細を見る</a>】";
                 document.getElementById("text_s").innerHTML = s;
+
+                //ノートとデスクトップ表示
+            } else if(ansers >= 51500) {
+                s = "dnpすげーはいぱ";
+                document.getElementById("text_q").innerHTML = s;
+                //次の選択肢
+                s = "【<a href = 'file:///C:/Users/koyama/Desktop/%E9%96%8B%E7%99%BA%E6%AE%B4%E6%89%93/group-work/html/index.html'>前のページに戻る</a>】";
+                s += "【<a href = 'javascript:setReady()'>同じ問題を最初から</a>】";
+                s += "【<a href = ''>詳細を見る</a>】";
+                document.getElementById("text_s").innerHTML = s;
+
+            } else if(ansers >= 51000) {
+                s = "dnpすげー";
+                document.getElementById("text_q").innerHTML = s;
+                //次の選択肢
+                s = "【<a href = 'file:///C:/Users/koyama/Desktop/%E9%96%8B%E7%99%BA%E6%AE%B4%E6%89%93/group-work/html/index.html'>前のページに戻る</a>】";
+                s += "【<a href = 'javascript:setReady()'>同じ問題を最初から</a>】";
+                s += "【<a href = ''>詳細を見る</a>】";
+                document.getElementById("text_s").innerHTML = s;
+
+            } else if(ansers >= 50000) {
+                s = "dnpそこそこぱそこん";
+                document.getElementById("text_q").innerHTML = s;
+                //次の選択肢
+                s = "【<a href = 'file:///C:/Users/koyama/Desktop/%E9%96%8B%E7%99%BA%E6%AE%B4%E6%89%93/group-work/html/index.html'>前のページに戻る</a>】";
+                s += "【<a href = 'javascript:setReady()'>同じ問題を最初から</a>】";
+                s += "【<a href = ''>詳細を見る</a>】";
+                document.getElementById("text_s").innerHTML = s;
+
+                //ノート表示
+            } else if(ansers >= 1500) {
+                s = "npすげーはいぱ";
+                document.getElementById("text_q").innerHTML = s;
+                //次の選択肢
+                s = "【<a href = 'file:///C:/Users/koyama/Desktop/%E9%96%8B%E7%99%BA%E6%AE%B4%E6%89%93/group-work/html/index.html'>前のページに戻る</a>】";
+                s += "【<a href = 'javascript:setReady()'>同じ問題を最初から</a>】";
+                s += "【<a href = ''>詳細を見る</a>】";
+                document.getElementById("text_s").innerHTML = s;
+
+            } else if(ansers >= 1000) {
+                s = "npすげー";
+                document.getElementById("text_q").innerHTML = s;
+                //次の選択肢
+                s = "【<a href = 'file:///C:/Users/koyama/Desktop/%E9%96%8B%E7%99%BA%E6%AE%B4%E6%89%93/group-work/html/index.html'>前のページに戻る</a>】";
+                s += "【<a href = 'javascript:setReady()'>同じ問題を最初から</a>】";
+                s += "【<a href = ''>詳細を見る</a>】";
+                document.getElementById("text_s").innerHTML = s;
+
             }else {
-                s = "ザコぱそ";
+                s = "npそこそこ";
                 document.getElementById("text_q").innerHTML = s;
                 //次の選択肢
                 s = "【<a href = 'file:///C:/Users/koyama/Desktop/%E9%96%8B%E7%99%BA%E6%AE%B4%E6%89%93/group-work/html/index.html'>前のページに戻る</a>】";
